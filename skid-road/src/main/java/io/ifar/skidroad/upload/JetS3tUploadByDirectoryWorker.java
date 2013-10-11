e71bff7fbbd88fab187899c51bccb0267d6b7564
Lorrin Nelson <lhn@mult.ifario.us>
2013-10-11 13:28:44 -0700

    Add JetS3tUploadByDirectoryWorker

---
+++ skid-road/src/main/java/io/ifar/skidroad/upload/JetS3tUploadByDirectoryWorker.java
@@ -0,0 +1,67 @@
+package io.ifar.skidroad.upload;
+
+import io.ifar.skidroad.LogFile;
+import io.ifar.skidroad.jets3t.JetS3tStorage;
+import io.ifar.skidroad.tracking.LogFileTracker;
+import org.jets3t.service.ServiceException;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.DateTimeFormatterBuilder;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * A variation of {@link JetS3tUploadWorker} which uploads into a directory structure that reflects the local parent
+ * directory of the {@link LogFile}.
+ *
+ * Log files are uploaded into a /<em>&lt;parent&gt;</em>/yyyy/MM/dd (implemented in {@link #determineArchiveURI(io.ifar.skidroad.LogFile)}). In
+ * the local tracker database they are assigned a <em>&lt;parent&gt;</em> group (implemented in {@link #determineArchiveGroup(io.ifar.skidroad.LogFile)}).
+ */
+public class JetS3tUploadByDirectoryWorker extends AbstractUploadWorker {
+    private final static DateTimeFormatter URI_FORMATTER = new DateTimeFormatterBuilder()
+            .appendYear(4,4)
+            .appendLiteral('/')
+            .appendMonthOfYear(2)
+            .appendLiteral('/')
+            .appendDayOfMonth(2)
+            .toFormatter();
+
+    private final URI uploadBasePath;
+    private final JetS3tStorage storage;
+
+    public JetS3tUploadByDirectoryWorker(LogFile logFile, LogFileTracker tracker, URI uploadBaseURI, JetS3tStorage storage) {
+        super(logFile, tracker);
+        this.uploadBasePath = uploadBaseURI;
+        this.storage = storage;
+    }
+
+    /**
+     * Returns name of immediate parent directory of the logFile (on the local file system)
+     */
+    private String getParent(LogFile logFile) {
+        return logFile.getPrepPath().getParent().getFileName().toString();
+    }
+
+    @Override
+    String determineArchiveGroup(LogFile logFile) {
+        return getParent(logFile);
+    }
+
+    @Override
+    URI determineArchiveURI(LogFile logFile) throws URISyntaxException {
+        StringBuilder stringBuilder = new StringBuilder(uploadBasePath.toString());
+        if (stringBuilder.charAt(stringBuilder.length()-1) != '/')
+            stringBuilder.append('/');
+        stringBuilder.append(getParent(logFile));
+        stringBuilder.append('/');
+        stringBuilder.append(URI_FORMATTER.print(logFile.getStartTime()));
+        stringBuilder.append('/');
+        stringBuilder.append(logFile.getPrepPath().getFileName().toString());
+        return new URI(stringBuilder.toString());
+    }
+
+    @Override
+    void push(LogFile logFile) throws ServiceException {
+        storage.put(logFile.getArchiveURI().toString(), logFile.getPrepPath().toFile());
+    }
+}
---
+++ skid-road/src/test/java/io/ifar/skidroad/upload/JetS3tUploadByDirectoryWorkerTest.java
@@ -0,0 +1,32 @@
+package io.ifar.skidroad.upload;
+
+import io.ifar.skidroad.LogFile;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.junit.Test;
+
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.junit.Assert.*;
+
+public class JetS3tUploadByDirectoryWorkerTest {
+
+    @Test
+    public void testDetermineArchiveURI() throws URISyntaxException {
+        LogFile logFile = new LogFile();
+        logFile.setStartTime(new DateTime(2013,10,7,21,33,57, DateTimeZone.UTC));
+        logFile.setPrepPath(new File("/var/skid_road_logs/environment_foo/my_project_2013-10-07T21-33_1.gz.aes-sic").toPath());
+        JetS3tUploadByDirectoryWorker worker = new JetS3tUploadByDirectoryWorker(logFile, null, new URI("s3://my_bucket/prefix"), null);
+        assertEquals(new URI("s3://my_bucket/prefix/environment_foo/2013/10/07/my_project_2013-10-07T21-33_1.gz.aes-sic"), worker.determineArchiveURI(logFile));
+    }
+
+    @Test
+    public void testDetermineArchiveGroup() {
+        LogFile logFile = new LogFile();
+        logFile.setPrepPath(new File("/var/skid_road_logs/environment_foo/my_project_2013-10-07T21-33_1.gz.aes-sic").toPath());
+        JetS3tUploadByDirectoryWorker worker = new JetS3tUploadByDirectoryWorker(logFile, null, null, null);
+        assertEquals("environment_foo", worker.determineArchiveGroup(logFile));
+    }
+}
