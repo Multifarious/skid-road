f3e563b37c5b0ea529847d01464649ea050116c5
Garrett Conaty <garrett@conaty.net>
2015-06-12 11:46:18 -0700

    ensure that the thread locals get set up before any other filters run

--- skid-road-jersey/src/main/java/io/ifar/skidroad/jersey/single/RequestTimestampFilter.java
+++ skid-road-jersey/src/main/java/io/ifar/skidroad/jersey/single/RequestTimestampFilter.java
@@ -3,12 +3,15 @@ package io.ifar.skidroad.jersey.single;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 
+import javax.annotation.Priority;
+import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 
 /**
  * Generates a unique UUID for this request and stashes it in a ThreadLocal for the duration of the request.
  */
+@Priority(Priorities.AUTHENTICATION - 100)
 public class RequestTimestampFilter implements ContainerRequestFilter {
     private static ThreadLocal<DateTime> requestTime = new ThreadLocal<>();
 
--- skid-road-jersey/src/main/java/io/ifar/skidroad/jersey/single/UUIDGeneratorFilter.java
+++ skid-road-jersey/src/main/java/io/ifar/skidroad/jersey/single/UUIDGeneratorFilter.java
@@ -1,5 +1,7 @@
 package io.ifar.skidroad.jersey.single;
 
+import javax.annotation.Priority;
+import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import java.util.UUID;
@@ -7,6 +9,7 @@ import java.util.UUID;
 /**
  * Generates a unique UUID for this request and stashes it in a ThreadLocal for the duration of the request.
  */
+@Priority(Priorities.AUTHENTICATION - 100)
 public class UUIDGeneratorFilter implements ContainerRequestFilter {
     private static ThreadLocal<String> requestID = new ThreadLocal<>();
 
